5289fb190bcf781f05caa5abecf932fa2558589a
CuongHoang <cuong.ca@outlook.com>
2020-09-23 00:29:16 -0300

    Changed to Pycham

--- HelloCuonghv.py
+++ HelloCuonghv.py
@@ -1,13 +1,15 @@
 print("Happy Birthday!!!")
-while True: 
-    
+while True:
     name = input("Please enter your name:")
     if name == "Cuong":
-        print("Hello "+name+", how are you today?")
+        print("Hello " + name + ", how are you today?")
     elif name == "Brian":
         print("Hello teacher, how are you today?")
-    elif name == "bye" or name == "quit" or name =="exit":
+    elif name == "bye" or name == "quit" or name == "exit":
         print("Quitting...")
         break
     else:
-        print("Sorry "+name+", you should not be here'")
+        print("Sorry " + name + ", you should not be here'")
+
+
+
